f0e7ce7611715eede47020a3b27d6563932221f2
kathleen koh <kathleen.khy@gmail.com>
2023-09-18 16:51:35 +0800

    adding receipt preview when uploading a receipt

---
+++ app/javascript/controllers/preview_image_uploaded_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="preview-image-uploaded"
+export default class extends Controller {
+  static targets= ["previewImage"]
+
+  change(event) {
+    const input = event.target;
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const preview = this.previewImageTarget;
+
+          const img = new Image();
+          img.src = e.target.result;
+          img.style.height = "500px";
+          preview.style.height = "500px";
+
+          preview.src = img.src;
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+  }
+}
