c7ed9ee8543d6828c2c9a771e3a7f5f40c3f9d15
catherine lim <jkvvhappy@gmail.com>
2023-09-21 14:19:32 +0800

    # before_validation :assign_or_create_member

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   belongs_to :member
 
   before_save :make_admin
-  before_validation :assign_or_create_member
+  # before_validation :assign_or_create_member
 
   def assign_or_create_member
     member = Member.find_or_create_by(phone_number:)
