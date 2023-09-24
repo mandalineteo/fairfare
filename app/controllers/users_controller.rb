2e3f6505ff5b55ac57cb39652e6979af30cf6698
kathleen koh <kathleen.khy@gmail.com>
2023-09-24 18:53:56 +0800

    adding user show page and attempting to allow edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      flash.now[:alert] = "Profile update failed."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name)
+  end
+end
